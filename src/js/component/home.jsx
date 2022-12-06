8e7005c5124c5515dcb23ca9394572f9aea14272
Keialex17 <hernandezkeiber34@gmail.com>
2022-12-06 23:55:29 +0000

    falta delete

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,66 +1,128 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 const ToDo = () => {
+  //Get a la lista de tareas
+
+  //Put para agregar una tarea
+  //Put para borrar una tarea
+
+  const initialValue = {
+    label: "",
+    done: false,
+  };
   const [taskList, setTaskList] = useState([]);
-  const [task, setTask] = useState("");
-  const [isShown, setIsShown] =useState(-1);
-  
-  
-  const handlertask = (event) => {
-    setTask(event.target.value);
+  const [task, setTask] = useState(initialValue);
+  const [isShown, setIsShown] = useState(-1);
+
+  const api = "https://assets.breatheco.de/apis/fake/todos/user/Keialex17";
+
+  const getTask = async () => {
+    const response = await fetch(api);
+    console.log(response);
+    if (response.ok) {
+      const data = await response.json();
+      setTaskList(data);
+    }
   };
 
-  const handlerKeyPress = (event) => {
+  const handlertask = (event) => {
+    setTask({
+      ...task,
+      [event.target.name]: event.target.value,
+    });
+  };
+
+  const handlerKeyPress = async (event) => {
     // event.preventDefault();
 
     if (event.key == "Enter" && task != "") {
-    
-        setTaskList([...taskList, task]);
-        setTask("");
-      
+      const responseAddMember = await fetch(api, {
+        method: "PUT", // or 'PUT'
+        body: JSON.stringify([...taskList, task]),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+      if (responseAddMember.ok) {
+        getTask();
+        setTask(initialValue);
+      }
     }
   };
-  const handlerButtomDelete = (indexid) => setTaskList(taskList.filter((tarea , index)=> (index != indexid)))
-            
+  const handlerButtomDelete = async (indexid) => {
+    const deleteList=  taskList.filter((tarea, index) => index != indexid)
+    const responseDeleteMember= await fetch(api, {
+      method: "PUT", // or 'PUT'
+      body: JSON.stringify(deleteList),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    if(responseDeleteMember.ok){
+      getTask()
+    }
+  };
+
+
+  useEffect(() => {
+    getTask();
+  }, []);
   return (
     <div className="container">
-    <div className="row vh-100 colores">
-      <div className="title d-flex justify-content-center pt-2">
-        <h1>Lista de tareas</h1>
-      </div>
-      <div className="col-3"></div>
-      <div className="col-6">
-        <div className="Card" id="card">
-          <div className="form-floating mb-3">
-            <input
-              onChange={handlertask}
-              value={task}
-              onKeyDown={handlerKeyPress}
-              type="text"
-              className="form-control  "
-              id="floatingInput"
-              placeholder="Tarea por hacer"
-            />
-            
-            <label htmlFor="floatingInput">No hay tareas, añadir tareas.</label>
-            <div className="task"></div>
-            {taskList.map((tarea, i) => {
-              return (
-              <span className="d-flex justify-content-between py-2 px-3 g-tareas text-black my-1
-              rounded-1 border border border-info" key={`s-${i}`}
-              onMouseEnter={() => {setIsShown(i)}} onMouseLeave={() => {setIsShown(-1)}} >
-                <h2 key={i}>{tarea}</h2> 
-               
-                { isShown == i &&
-                <i className="fas fa-minus-circle mt-3 ms-4 position-relative me-3 " 
-                key={`p-${i}`} onClick={() => {handlerButtomDelete(i)}}></i>}
-              </span>);
-              
-            })}
+      <div className="row vh-100 colores">
+        <div className="title d-flex justify-content-center pt-2">
+          <h1>Lista de tareas</h1>
+        </div>
+        <div className="col-3"></div>
+        <div className="col-6">
+          <div className="Card" id="card">
+            <div className="form-floating mb-3">
+              <input
+                onChange={handlertask}
+                value={task.label}
+                onKeyDown={handlerKeyPress}
+                type="text"
+                className="form-control  "
+                id="floatingInput"
+                placeholder="Tarea por hacer"
+                name="label"
+              />
+
+              <label htmlFor="floatingInput">
+                No hay tareas, añadir tareas.
+              </label>
+              <div className="task"></div>
+              {taskList.map((tarea, i) => {
+                return (
+                  <span
+                    className="d-flex justify-content-between py-2 px-3 g-tareas text-black my-1
+              rounded-1 border border border-info"
+                    key={`s-${i}`}
+                    onMouseEnter={() => {
+                      setIsShown(i);
+                    }}
+                    onMouseLeave={() => {
+                      setIsShown(-1);
+                    }}
+                  >
+                    <h2 key={i}>{tarea.label}</h2>
+
+                    {isShown == i && (
+                      <i
+                        className="fas fa-minus-circle mt-3 ms-4 position-relative me-3 "
+                        key={`p-${i}`}
+                        onClick={() => {
+                          handlerButtomDelete(i);
+                        }}
+                      ></i>
+                    )}
+                  </span>
+                );
+              })}
+            </div>
           </div>
         </div>
       </div>
-    </div>
     </div>
   );
 };
